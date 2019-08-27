327e9da74901e68eadf5334bde964d652ae38d82
Kos Huang <koshuang@gmail.com>
2019-08-27 11:11:42 +0800

    fix when no hooks

--- git/templates/hooks/run-hooks.sh
+++ git/templates/hooks/run-hooks.sh
@@ -6,18 +6,19 @@ repo=$( git rev-parse --show-toplevel )
 hook_type=$( basename $0 )
 hooks=~/.dotfiles/git/hooks
 
-echo "Executing $hook_type hook(s)"
-
 for hook in $hooks/*.$hook_type; do
-	echo ""
-	echo "${COLOR_LIGHTPURPLE}Executing ${hook}${COLOR_NONE}"
-	${hook}
-	EXIT_CODE=$((${EXIT_CODE} + $?))
+        if [ -f "$hook" ]
+        then
+                echo ""
+                echo "${COLOR_LIGHTPURPLE}Executing ${hook}${COLOR_NONE}"
+                ${hook}
+                EXIT_CODE=$((${EXIT_CODE} + $?))
+        fi
 done
 
-if [[ ${EXIT_CODE} -ne 0 ]]; then
-	echo ""
-	echo "${COLOR_RED}Commit Failed.${COLOR_NONE}"
+if [ ${EXIT_CODE} -ne 0 ]; then
+        echo ""
+        echo "${COLOR_RED}Commit Failed.${COLOR_NONE}"
 fi
 
 exit $((${EXIT_CODE}))
